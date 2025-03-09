0a92e349c7c027b5b5e4df1fffcbfce7283bc28a
Jillian Kupchella <jt0898@me.com>
2025-03-09 17:54:09 -0600

    update static method

--- app/message_handler.py
+++ app/message_handler.py
@@ -25,7 +25,7 @@ class MessageHandler:
         self.start_time = time.time()
 
     @staticmethod
-    def _voltage_request(self):
+    def _voltage_request():
         return can.Message(
             arbitration_id=0x7DF,
             data=[0x02, 0x01, 0x42],
@@ -33,7 +33,7 @@ class MessageHandler:
         )
 
     @staticmethod
-    def _runtime_request(self):
+    def _runtime_request():
         return can.Message(
             arbitration_id=0x7DF, data=[0x02, 0x01, 0x1F], is_extended_id=False
         )
@@ -42,7 +42,7 @@ class MessageHandler:
         """Reads the CAN bus."""
         # Send the request message
         try:
-            self.bus.send(self._runtime_request())
+            self.bus.send(_runtime_request())
             logger.info("OBD-II request sent successfully.")
         except can.CanError as e:
             logger.error("Error sending OBD-II request.")
