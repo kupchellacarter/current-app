ec260c2f0d83bf1ee3256fb1fadbba51b6fca98c
carter <kupchellacarter@gmail.com>
2025-03-08 18:24:09 -0700

    message

--- app/python_can_read.py
+++ app/python_can_read.py
@@ -1,22 +1,14 @@
 import can
 
+# Set up the CAN interface (replace 'can0' with the correct interface if needed)
 bus = can.interface.Bus(channel='can0', bustype='socketcan')
 
-# Send OBDII request for Pack Voltage (PID 0x22dd83)
-request = can.Message(arbitration_id=0x7DF,  # OBDII broadcast address
-                      data=[0x02, 0x22, 0xDD, 0x83, 0x00, 0x00, 0x00, 0x00],
-                      is_extended_id=False)
+# OBD-II request message
+message = can.Message(arbitration_id=0x7DF, data=[0x02, 0x01, 0x1F], is_extended_id=False)
 
-print("Sending OBDII request for Pack Voltage...")
-bus.send(request)
-
-# Listen for the response
-while True:
-    response = bus.recv(timeout=1.0)
-    if response and response.arbitration_id == 0x7E8:
-        print("Received OBDII Response:", response.data)
-        break
-
-
-
-
+# Send the message
+try:
+    bus.send(message)
+    print("OBD-II request sent successfully.")
+except can.CanError:
+    print("Error sending OBD-II request.")
