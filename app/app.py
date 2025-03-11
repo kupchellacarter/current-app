c7837973e7ac3c8fd144688c384c72adbbaa625d
Jillian Kupchella <jt0898@me.com>
2025-03-10 21:16:03 -0600

    test

--- app/app.py
+++ app/app.py
@@ -19,18 +19,22 @@ class App:
         """
         queries data and updates GUI
         """
+        last_display = None
         while True:
             # self.charging_mode = self.can_opener.listen_for_charge_messages()
             if self.charging_mode:
-                self.display_charging_ui()
+                self.display_charging_ui(last_display)
+                last_display = "defualt_ui"
             else:
                 self.display_default_ui()
+                last_display = "charging_ui"
 
-    def display_default_ui(self):
+    def display_default_ui(self,last_display:str):
         """
         displays default UI
         """
-        self.gui.display_defualt_ui()
+        if not last_display or last_display != "defualt_ui":
+            self.gui.display_defualt_ui()
         self.obd2_handler.request_and_parse("runtime")
         runtime = self.obd2_handler.get_runtime()
         self.obd2_handler.request_and_parse("voltage")
