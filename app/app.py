6ce55ccaf04a4477bddb358b371bc08e610b6a91
Carter Kupchella <kupchellacarter@gmail.com>
2025-06-02 19:08:27 -0600

    add error checking

--- app/app.py
+++ app/app.py
@@ -48,42 +48,43 @@ class App:
         # PGN 0xFF20 MCU_Summary
         self.data = self.dbc_handler.dbc_request_and_parse(self.dbc_request.mcu_summary)
 
-        mcu_chargedenergy = (
-            self.data.mcu_chargedenergy * self.data.mcu_chargedenergy_factor
-        )
-        mcu_chargedenergy_labelled = (
-            f"{mcu_chargedenergy} {self.data.mcu_chargedenergy_unit}"
-        )
-        if self.data.mcu_chargestate.value == 1:
-            mcu_chargestate = "Charging"
-        elif self.data.mcu_chargestate.value == 2:
-            mcu_chargestate = "Warmdown"
-        else:
-            mcu_chargestate = "Charge State Unknown"
+        if self.data:
+            mcu_chargedenergy = (
+                self.data.mcu_chargedenergy * self.data.mcu_chargedenergy_factor
+            )
+            mcu_chargedenergy_labelled = (
+                f"{mcu_chargedenergy} {self.data.mcu_chargedenergy_unit}"
+            )
+            if self.data.mcu_chargestate.value == 1:
+                mcu_chargestate = "Charging"
+            elif self.data.mcu_chargestate.value == 2:
+                mcu_chargestate = "Warmdown"
+            else:
+                mcu_chargestate = "Charge State Unknown"
 
-        if self.data.mcu_plugstate.value == 1:
-            mcu_plugstate = "No Plug"
-        elif self.data.mcu_plugstate.value == 2:
-            mcu_plugstate = "Connected"
-        else:
-            mcu_plugstate = "Plug State Unknown"
+            if self.data.mcu_plugstate.value == 1:
+                mcu_plugstate = "No Plug"
+            elif self.data.mcu_plugstate.value == 2:
+                mcu_plugstate = "Connected"
+            else:
+                mcu_plugstate = "Plug State Unknown"
 
         system_errors = []
         # if self.data.mcu_fault_notlocked:
         #    system_errors.append("MCU Fault: Not Locked")
-        if self.data.mcu_fault_thermundertemp:
+        if self.data and self.data.mcu_fault_thermundertemp:
             system_errors.append("Low Temp!")
-        if self.data.mcu_fault_thermovertemp:
+        if self.data and self.data.mcu_fault_thermovertemp:
             system_errors.append("High Temp!")
-        if self.data.mcu_fault_lvc:
+        if self.data and self.data.mcu_fault_lvc:
             system_errors.append("Low Voltage!")
-        if self.data.mcu_fault_hvc:
+        if self.data and self.data.mcu_fault_hvc:
             system_errors.append("High Voltage!")
         # if self.data.mcu_fault_thermcensus:
         # system_errors.append("MCU Fault: Therm Census")
         # if self.data.mcu_fault_cellcensus:
         # system_errors.append("MCU Fault: Cell Census")
-        if self.data.mcu_fault_hardware:
+        if self.data and self.data.mcu_fault_hardware:
             system_errors.append("Hardware Fault")
         # if self.data.mcu_fault_illegalconfig:
         #    system_errors.append("MCU Fault: Illegal Configuration")
@@ -92,58 +93,63 @@ class App:
         self.data = self.dbc_handler.dbc_request_and_parse(
             self.dbc_request.mcu_pack_summary
         )
-        pack_voltage = round(
-            self.data.mcu_packvoltage * self.data.mcu_packvoltage_factor, 2
-        )
-        pack_voltage_labelled = f"{pack_voltage} {self.data.mcu_packvoltage_unit}"
-        pack_current = round(
-            self.data.mcu_packcurrent * self.data.mcu_packcurrent_factor, 2
-        )
-        pack_current_labelled = f"{pack_current} {self.data.mcu_packcurrent_unit}"
-        pack_kw = round(pack_voltage * pack_current / 1000, 2)
-        pack_kw_labelled = f"{pack_kw} {self.data.mcu_packkw_unit}"
+        if self.data:
+            pack_voltage = round(
+                self.data.mcu_packvoltage * self.data.mcu_packvoltage_factor, 2
+            )
+            pack_voltage_labelled = f"{pack_voltage} {self.data.mcu_packvoltage_unit}"
+            pack_current = round(
+                self.data.mcu_packcurrent * self.data.mcu_packcurrent_factor, 2
+            )
+            pack_current_labelled = f"{pack_current} {self.data.mcu_packcurrent_unit}"
+            pack_kw = round(pack_voltage * pack_current / 1000, 2)
+            pack_kw_labelled = f"{pack_kw} {self.data.mcu_packkw_unit}"
 
         # PGN 0xFF22 MCU_CellSummary
         self.data = self.dbc_handler.dbc_request_and_parse(
             self.dbc_request.mcu_cell_summary
         )
-        cell_count = self.data.mcu_cellcount
-        mean_cell_v = round(self.data.mcu_meancellv * self.data.mcu_meancellv_factor, 2)
-        mean_cell_v_labelled = f"{mean_cell_v} {self.data.mcu_meancellv_unit}"
+        if self.data:
+            cell_count = self.data.mcu_cellcount
+            mean_cell_v = round(self.data.mcu_meancellv * self.data.mcu_meancellv_factor, 2)
+            mean_cell_v_labelled = f"{mean_cell_v} {self.data.mcu_meancellv_unit}"
 
         # PGN 0xFF23 MCU_ThermSummary
         self.data = self.dbc_handler.dbc_request_and_parse(
             self.dbc_request.mcu_therm_summary
         )
-        highest_therm = self.data.mcu_thhighest
+        if self.data:
+            highest_therm = self.data.mcu_thhighest
 
         # PGN 0xFF24 MCU_SOCSummary
         self.data = self.dbc_handler.dbc_request_and_parse(
             self.dbc_request.mcu_soc_summary
         )
-        soc = self.data.mcu_soc
-        pack_cur_kwh = self.data.mcu_packcurkwh * self.data.mcu_packcurkwh_factor
-        pack_cur_kwh_labelled = f"{pack_cur_kwh} {self.data.mcu_packcurkwh_unit}"
-        pack_max_kwh = self.data.mcu_packmaxkwh * self.data.mcu_packmaxkwh_factor
-        pack_max_kwh_labelled = f"{pack_max_kwh} {self.data.mcu_packmaxkwh_unit}"
+        if self.data:
+            soc = self.data.mcu_soc
+            pack_cur_kwh = self.data.mcu_packcurkwh * self.data.mcu_packcurkwh_factor
+            pack_cur_kwh_labelled = f"{pack_cur_kwh} {self.data.mcu_packcurkwh_unit}"
+            pack_max_kwh = self.data.mcu_packmaxkwh * self.data.mcu_packmaxkwh_factor
+            pack_max_kwh_labelled = f"{pack_max_kwh} {self.data.mcu_packmaxkwh_unit}"
 
         self.data = self.dbc_handler.dbc_request_and_parse(self.dbc_request.bms_config1)
-        hvc = round(self.data.bms_hvc * self.data.bms_hvc_factor, 2)
-        lvc = round(self.data.bms_lvc * self.data.bms_lvc_factor, 2)
+        if self.data:
+            hvc = round(self.data.bms_hvc * self.data.bms_hvc_factor, 2)
+            lvc = round(self.data.bms_lvc * self.data.bms_lvc_factor, 2)
 
         self.gui.update_error_label(system_errors)
-        self.gui.set_soc(soc)
-        self.gui.set_cell_voltage_slider(mean_cell_v, lvc, hvc)
-        self.gui.set_pack_kwh(pack_cur_kwh, pack_max_kwh_labelled)
+        self.gui.set_soc(soc or "")
+        self.gui.set_cell_voltage_slider(mean_cell_v or 0, lvc or 0, hvc or 0)
+        self.gui.set_pack_kwh(pack_cur_kwh or "", pack_max_kwh_labelled or "")
 
-        self.gui.set_pack_voltage(pack_voltage_labelled)
-        self.gui.set_pack_current(pack_current_labelled)
-        self.gui.set_power(pack_kw_labelled)
+        self.gui.set_pack_voltage(pack_voltage_labelled or "")
+        self.gui.set_pack_current(pack_current_labelled or "")
+        self.gui.set_power(pack_kw_labelled or "")
 
-        self.gui.set_cell_count(cell_count)
-        self.gui.set_cell_voltage(mean_cell_v_labelled)
+        self.gui.set_cell_count(cell_count or "")
+        self.gui.set_cell_voltage(mean_cell_v_labelled or "")
 
-        self.gui.set_temp(highest_therm)
+        self.gui.set_temp(highest_therm or "")
 
         self.gui.update_runtime(runtime)
         self.gui.refresh_ui()
